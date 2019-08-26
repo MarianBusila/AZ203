398a6d741be3e0c38ab25703d94d80f8396c8eae
Marian Busila <marian.busila@gmail.com>
2019-08-25 23:24:13 -0400

    add output binding

--- PaaS/WebJobsSDKSample/Functions.cs
+++ PaaS/WebJobsSDKSample/Functions.cs
@@ -10,9 +10,11 @@ namespace WebJobsSDKSample
         public static void ProcessQueueMessage(
             [QueueTrigger("queue")] string message, 
             [Blob("container/{queueTrigger}", FileAccess.Read)] Stream myBlob,
+            [Blob("container/copy-{queueTrigger}", FileAccess.Write)] Stream outputBlob,
             ILogger logger)
         {
             logger.LogInformation($"Blob name: {message} \n Size: {myBlob.Length} bytes");
+            myBlob.CopyTo(outputBlob);
 
         }
     }
